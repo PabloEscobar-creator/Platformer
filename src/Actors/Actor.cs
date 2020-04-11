bc2d52352f7bca38b73302239eea383db4ab5774
Paweł <p.gogolok@gmail.com>
2020-04-11 14:53:53 +0200

    Dodany bohater z grawitacją

--- src/Actors/Actor.cs
+++ src/Actors/Actor.cs
@@ -3,10 +3,20 @@ using System;
 
 public class Actor : KinematicBody2D
 {
-    var gravity = 3000.0;
-    
-    void _physics_process(float delta){
+     Vector2 velocity;
+     [Export]
+     public Vector2 speed = new Vector2(300, 1000);
+         
+     [Export] 
+     public float gravity = 3000;
 
-    }
-    
+     public override void _PhysicsProcess(float delta)
+     {
+          velocity.y += gravity * delta;
+          //if (velocity.y > speed.y)
+         // {
+           //    velocity.y = speed.y;
+         // }
+          velocity = MoveAndSlide(velocity);
+     }
 }
